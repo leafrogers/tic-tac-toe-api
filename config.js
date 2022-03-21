93df849334a9a8451515068aec840880e65a1e59
Leaf Rogers <150157+leafrogers@users.noreply.github.com>
2022-03-21 16:33:45 +0000

    refactor(routes): remove example route to make way for the real tic-tac-toe ones
    BREAKING CHANGE: The Pokemon route will no longer be served

--- config.js
+++ config.js
@@ -1,5 +1,4 @@
 export default {
-	API_URL: process.env.API_URL || 'https://pokeapi.co/api/v2',
 	APP_FRIENDLY_NAME: 'Tic-tac-toe API',
 	PORT: process.env.PORT || 3000
 };
--- server/app.js
+++ server/app.js
@@ -1,10 +1,8 @@
 import express from 'express';
 import helmet from 'helmet';
-import pokemonController from './pages/pokemon/controller.js';
 
 const app = express();
 
 app.use(helmet());
-app.get('/', pokemonController);
 
 export default app;
